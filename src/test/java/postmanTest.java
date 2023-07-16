57f55c1542468ea8ab1eecfb074d255f52de165f
Pers test <qa61test@gmail.com>
2023-07-17 00:17:10 +0400

    initial commit

---
+++ src/test/java/postmanTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class postmanTest {
+
+    // Given - When - Then
+    // Предусловия
+    @Test
+    void postTest() {
+
+        given()
+            .baseUri("https://postman-echo.com")
+            .contentType("text/plain; charset=UTF-8")
+            .body("провальный тест") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+            .when()
+            .post("/post")
+// Проверки
+            .then()
+            .statusCode(200)
+            .body("data", equalTo("тест"));
+    }
+}
